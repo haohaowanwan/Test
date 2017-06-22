b8054c50ae94dbb50314f45da3884a3171ff48ef
Meng Fan <niumengfan@qichegou.com>
2017-06-22 16:27:31 +0800

    第一次提交

---
+++ TestDemo/TestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestDemo
+//
+//  Created by Meng Fan on 2017/6/22.
+//  Copyright © 2017年 Haowan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestDemo/TestDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TestDemo
+//
+//  Created by Meng Fan on 2017/6/22.
+//  Copyright © 2017年 Haowan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
